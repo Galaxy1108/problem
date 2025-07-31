5cddd09376f744444afc02332cf2119ff1a04b3e
Galaxy_1108 <int_256t@outlook.com>
2025-07-31 21:52:47 +0800

    0719 联赛难度综合复习巩固

---
+++ P3177.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+struct edges {
+    ll v, w;
+};
+vector<edges> a[2005];
+ll dp[2005][2005];
+ll sub[2005], n, k;
+
+void DP(ll u, ll f) {
+    sub[u] = 1;
+    dp[u][0] = dp[u][1] = 0;
+    for (auto its : a[u]) {
+        ll v = its.v, w = its.w;
+        if (v == f) {
+            continue;
+        }
+        DP(v, u);
+        sub[u] += sub[v];
+        // cout << min(k, sub[u]) << "\n\n";
+        for (int j = min(k, sub[u]);j >= 0;j--) {
+            if (j == 0 || j == 1) {
+                dp[u][j] += dp[v][0] + (1ll * sub[v] * (n - k - sub[v])) * w;
+                cout << dp[u][j] <<' ' << sub[v] <<' '<<n-k-sub[v]<<' '<<w << " A\n";
+                // continue;
+            }
+            for (int sub_j = min(j * 1ll, sub[v]); sub_j > 0;sub_j--) {
+                // cout << j << endl;
+                if (dp[u][j - sub_j] == LLONG_MIN / 2) {
+                    continue;
+                }
+                ll tot = 1ll * sub_j * (k - sub_j) + 1ll * (sub[v] - sub_j) * (n - k - sub[v] + sub_j);
+                cout << tot << '\n';
+                dp[u][j] = max(dp[u][j], dp[u][j - sub_j] + dp[v][sub_j] + 1ll * tot * w);
+                cout << dp[u][j] << ' ' << dp[v][sub_j] <<' '<<tot*w << '\n';
+            }
+        }
+    }
+}
+
+int main() {
+    freopen("P3177.out", "w", stdout);
+    cin >> n >> k;
+    for (int i = 1;i < n;i++) {
+        ll u, v, w;
+        cin >> u >> v >> w;
+        // w = (w == 1 ? 2 : 1);
+        a[u].emplace_back((edges) { v, w });
+        a[v].emplace_back((edges) { u, w });
+    }
+    for (int i = 0;i <= n;i++) {
+        for (int j = 0;j <= n;j++) {
+            dp[i][j] = LLONG_MIN / 2;
+        }
+    }
+    DP(1, -1);
+    cout << dp[1][k] << '\n';
+    return 0;
+}
